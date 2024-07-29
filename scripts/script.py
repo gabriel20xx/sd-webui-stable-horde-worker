725ba696a7f52c1bcd697d8bc66a006e89bc5d0b
gabriel20xx <gabrielfranz31@gmail.com>
2024-07-29 20:49:18 +0200

    fix

--- scripts/script.py
+++ scripts/script.py
@@ -543,6 +543,8 @@ def get_news_ui():
                     details.append(message)
 
                     tags_value = news_item.get('tags', 'No tags available')
+                    if tags_value == 'No tags available':
+                        tags_value = []
                     tags_string = ', '.join(map(str, tags_value))
                     stripped_tags_value = tags_string.replace("'", "").replace("[", "").replace("]", "")
                     tags = gr.Textbox(

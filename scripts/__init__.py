e02c9af286e8be4714898c565be4aa8871b1f88b
gabriel20xx <gabrielfranz31@gmail.com>
2024-08-16 13:43:54 +0200

    fix

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,5 +1,7 @@
 from .script import fetch_api_info
+from .subpages import get_worker_ui
 
 __all__ = [
-    "fetch_api_info"
+    "fetch_api_info",
+    "get_worker_ui",
 ]
--- scripts/subpages/__init__.py
+++ scripts/subpages/__init__.py
@@ -1,5 +1,7 @@
-from subpages.worker_ui import get_worker_ui
+from .worker_ui import get_worker_ui
+from script import fetch_api_info
 
 __all__ = [
     "get_worker_ui",
+    "fetch_api_info",
 ]
